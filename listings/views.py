a485975420205028633bcd613c20494b8b276038
grebenik999 <vg.trade999@gmail.com>
2019-02-25 09:45:42 +0200

    added search option to the site

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 
+from .choices import bedroom_choices, price_choises, state_choises
 from .models import Listing
 
 
@@ -15,8 +17,49 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    return render(request, 'listings/listing.html', {'listing': listing})
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Search by Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # Search by City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # Search by State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Search by Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Search by Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choises': price_choises,
+        'state_choises': state_choises,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choises, state_choises
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'price_choises': price_choises,
+        'state_choises': state_choises
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    print('mvp', mvp_realtors)
+
+    return render(request, 'pages/about.html', {'realtors': realtors, 'mvp_realtors': mvp_realtors})
